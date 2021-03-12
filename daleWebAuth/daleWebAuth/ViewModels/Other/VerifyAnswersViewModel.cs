236ae811774077c6a7e8c15436a744163e1f244c
Muhammad Nadeem <muhammad.nadeem@hashmakersol.com>
2021-03-12 15:20:47 +0500

    improvements

--- daleWebAuth/daleWebAuth/ViewModels/Other/VerifyAnswersViewModel.cs
+++ daleWebAuth/daleWebAuth/ViewModels/Other/VerifyAnswersViewModel.cs
@@ -1,7 +1,11 @@
-﻿using daleWebAuth.ViewModels.Base;
+﻿using daleWebAuth.Pages;
+using daleWebAuth.ViewModels.Base;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace daleWebAuth.ViewModels.Other
 {
@@ -10,6 +14,14 @@ namespace daleWebAuth.ViewModels.Other
         public VerifyAnswersViewModel()
         {
 
+        }
+
+        ICommand _verifyCommand;
+        public ICommand DoneCommand => _verifyCommand ??= new AsyncCommand(ExecuteDoneCommand);
+
+        public async Task ExecuteDoneCommand()
+        {
+            await NavigationService.PushAsync(new Pages.Other.SuccessPage());
         }
     }
 }
